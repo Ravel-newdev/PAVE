f0f3e7dbd719bc44e4afbe4d63c70ffffd446a4c
FelipeGabriel05 <felpsgb2321@gmail.com>
2026-06-04 11:26:04 -0300

    instalando dependências e criando rotas

--- frontend/src/lib/services/constants.ts
+++ frontend/src/lib/services/constants.ts
@@ -1,10 +1,10 @@
-// import { QueryClient } from "@tanstack/react-query";
-// import axios from "axios";
+import { QueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
-// export const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
-// export const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_BASE_URL,
-//     timeout: 3000,
-//     withCredentials: true,
-// });
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    timeout: 3000,
+    withCredentials: true,
+});
