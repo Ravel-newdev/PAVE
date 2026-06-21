966bedc0dcfd1d48d39728e069443eedf0b664fa
FelipeGabriel05 <felpsgb2321@gmail.com>
2026-06-21 10:59:13 -0300

    fix: inclusão de apenas um estilo css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-import './index.css';
+import './styles.css';
 
 import { routeTree } from './routeTree.gen';
 
